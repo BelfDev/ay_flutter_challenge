48eb447c736ce6e2d58503863e6e3056c097073a
Pedro Belfort <belfortdev@gmail.com>
2020-08-20 00:24:48 +0100

    Add initial route configuration property

--- lib/configs/app_config.dart
+++ lib/configs/app_config.dart
@@ -1,3 +1,6 @@
+import 'package:ay_flutter_challenge/routes/routes.dart';
+
 abstract class AppConfig {
   static const String appTitle = 'Contacts';
+  static const String initialRoute = ContactListRoute.id;
 }
