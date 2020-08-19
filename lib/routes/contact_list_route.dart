2fa74a396130e18305b9fdff1eb370f5da4eb4a2
Pedro Belfort <belfortdev@gmail.com>
2020-08-20 00:25:25 +0100

    Add a RaisedButton in ContactListRoute for demo purposes

--- lib/routes/contact_list_route.dart
+++ lib/routes/contact_list_route.dart
@@ -1,16 +1,30 @@
-import 'package:ay_flutter_challenge/configs/app_theme.dart';
+import 'package:ay_flutter_challenge/configs/configs.dart';
 import 'package:flutter/material.dart';
 
+import 'contact_detail_route.dart';
+
 class ContactListRoute extends StatelessWidget {
+  static const String id = '/contact-list';
+
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text(
-            'Home Route',
-            style: theme.textTheme.headline1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Home Route',
+                style: theme.textTheme.headline1,
+              ),
+              RaisedButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(ContactDetail.id),
+                child: Text('Touch Me!'),
+              )
+            ],
           ),
         ),
       ),
