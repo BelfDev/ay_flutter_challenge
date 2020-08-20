554ef4ed5dffb84cc7fb4ca4af22963ae90abcf4
Pedro Belfort <belfortdev@gmail.com>
2020-08-20 11:52:50 +0100

    Fix fetchContacts

--- lib/data/repositories/contact_repository.dart
+++ lib/data/repositories/contact_repository.dart
@@ -2,11 +2,12 @@ import 'package:ay_flutter_challenge/data/models/models.dart';
 import 'package:ay_flutter_challenge/data/source/local_data_source.dart';
 import 'package:ay_flutter_challenge/utils/exceptions/exceptions.dart';
 import 'package:ay_flutter_challenge/utils/extensions/string_operations_extension.dart';
+import 'package:flutter/cupertino.dart';
 
 /// Single source of truth for fetching contact data.
 /// The repository may retrieve data from a local or remote data source.
 class ContactRepository {
-  static const futureDelay = 200; // milliseconds
+  static const futureDelay = 400; // milliseconds
   static final ContactRepository _instance = ContactRepository._internal();
 
   factory ContactRepository() => _instance;
@@ -15,10 +16,15 @@ class ContactRepository {
 
   /// Returns a Future<List<Contact>> with all available contacts.
   Future<List<Contact>> fetchContacts() async {
-    final List<Contact> contacts =
-        LocalDataSource.contacts.map((entry) => _parseContact(entry));
-
-    return Future.delayed(Duration(milliseconds: futureDelay), () => contacts);
+    try {
+      final List<Contact> contacts = LocalDataSource.contacts
+          .map((entry) => _parseContact(entry))
+          .toList();
+      return Future.delayed(
+          Duration(milliseconds: futureDelay), () => contacts);
+    } catch (e) {
+      debugPrint(e);
+    }
   }
 
   Contact _parseContact(String localEntry) {
