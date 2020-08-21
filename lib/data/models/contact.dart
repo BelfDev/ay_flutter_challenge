cba2ad0d73f6bbb768befff73bea5428fc22fb19
Pedro Belfort <belfortdev@gmail.com>
2020-08-21 15:15:43 +0200

    Implement the Comparable interface in Contact model

--- lib/data/models/contact.dart
+++ lib/data/models/contact.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/material.dart';
-
 /// A data model class representing the Contact entity.
 /// The model contains convenience properties to access
 /// the contact's full name, first name's initial, or
 /// last name's initial.
-class Contact {
+class Contact implements Comparable<Contact> {
   final String firstName;
   final String lastName;
 
-  const Contact({@required this.firstName, @required this.lastName})
+  const Contact({this.firstName, this.lastName})
       : assert(firstName != null),
         assert(lastName != null);
 
@@ -16,8 +14,14 @@ class Contact {
   String get fullName => '$firstName $lastName';
 
   /// Returns the first character of the first name.
-  String get firstNameInitial => firstName[0];
+  String get firstNameInitial => firstName[0].toUpperCase();
 
   /// Returns the first character of the last name.
-  String get lastNameInitial => lastName[0];
+  String get lastNameInitial => lastName[0].toUpperCase();
+
+  @override
+  int compareTo(Contact other) => this.fullName.compareTo(other.fullName);
+
+  @override
+  String toString() => this.fullName;
 }
