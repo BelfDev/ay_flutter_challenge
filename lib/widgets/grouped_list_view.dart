4b7c99fe6d4c7303d1cbc53e904ecea3e4dbb3e3
Pedro Belfort <belfortdev@gmail.com>
2020-08-21 15:56:12 +0200

    Introduce FutureBuilder

--- lib/widgets/grouped_list_view.dart
+++ lib/widgets/grouped_list_view.dart
@@ -1,33 +1,51 @@
 import 'package:ay_flutter_challenge/configs/configs.dart';
+import 'package:ay_flutter_challenge/data/models/models.dart';
+import 'package:ay_flutter_challenge/data/repositories/contact_repository.dart';
 import 'package:flutter/material.dart';
 
 class GroupedListView extends StatelessWidget {
+  final repo = ContactRepository();
+
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return Builder(builder: (BuildContext context) {
-      return CustomScrollView(
-        // The "controller" and "primary" members should be left
-        // unset, so that the NestedScrollView can control this
-        // inner scroll view.
-        // If the "controller" property is set, then this scroll
-        // view will not be associated with the NestedScrollView.
-        slivers: <Widget>[
-          SliverOverlapInjector(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context)),
-          SliverFixedExtentList(
-            itemExtent: 48.0,
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) => ListTile(
-                  title: Text(
-                'Item $index',
-                style: theme.textTheme.headline1,
-              )),
-              childCount: 30,
-            ),
-          ),
-        ],
-      );
-    });
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverOverlapInjector(
+            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context)),
+        FutureBuilder<List<Contact>>(
+            future: repo.fetchContacts(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final contacts = snapshot.data;
+                return _buildContactList(contacts, theme);
+              } else if (snapshot.hasError) {
+                return SliverToBoxAdapter(
+                    child: Text('Something went wrong!',
+                        style: theme.textTheme.headline1));
+              } else {
+                return SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 100,
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+            }), //          SliverPersistentHeader(delegate: MyCustomDelegate())
+      ],
+    );
+  }
+
+  Widget _buildContactList(List<Contact> contacts, ThemeData theme) {
+    return SliverFixedExtentList(
+      itemExtent: 56.0,
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return ListTile(
+            title: Text(
+          '${contacts[index].fullName}',
+          style: theme.textTheme.headline1,
+        ));
+      }, childCount: contacts.length),
+    );
   }
 }
