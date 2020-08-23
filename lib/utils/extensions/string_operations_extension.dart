a18171b4b2b61d1cf86cd0cba2ac80b8f411dd7f
Pedro Belfort <belfortdev@gmail.com>
2020-08-23 02:23:45 -0300

    Add splitAtFirstWord extension method

--- lib/utils/extensions/string_operations_extension.dart
+++ lib/utils/extensions/string_operations_extension.dart
@@ -1,9 +1,9 @@
 /// Utility extension which adds convenience operations to the String object.
 extension StringOperations on String {
-  /// Returns true if this string contains non-empty content.
+  /// Returns true if this [String] contains non-empty content.
   bool get isNotNullNorEmpty => this != null && this.isNotEmpty;
 
-  /// Returns the String with capitalized first character.
+  /// Returns the [String] with capitalized first character.
   String toCapitalCase() {
     if (this == null) {
       throw ArgumentError("string: $this");
@@ -14,5 +14,16 @@ extension StringOperations on String {
     }
 
     return this[0].toUpperCase() + this.substring(1);
+  }
+
+  /// Returns a [List] with two [String]s. The first element contains the first
+  /// word and the second contains the remaining words of the given [String].
+  /// Throws [ArgumentError] if the provided [String] is empty.
+  List<String> splitAtFirstWord() {
+    final words = this.split(' ');
+    if (words.isNotEmpty) {
+      return [words[0], words.sublist(1)?.join(" ")?.toCapitalCase() ?? ''];
+    }
+    throw ArgumentError("string is empty: $this");
   }
 }
