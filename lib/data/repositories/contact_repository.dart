185f9cfd5a50e1061b650764886070e270c9917c
Pedro Belfort <belfortdev@gmail.com>
2020-08-23 02:24:54 -0300

    Adapt contact repository to use new models

--- lib/data/repositories/contact_repository.dart
+++ lib/data/repositories/contact_repository.dart
@@ -1,7 +1,5 @@
 import 'package:ay_flutter_challenge/data/models/models.dart';
 import 'package:ay_flutter_challenge/data/source/local_data_source.dart';
-import 'package:ay_flutter_challenge/utils/exceptions/exceptions.dart';
-import 'package:ay_flutter_challenge/utils/extensions/string_operations_extension.dart';
 import 'package:flutter/cupertino.dart';
 
 /// Single source of truth for fetching contact data.
@@ -15,26 +13,16 @@ class ContactRepository {
   ContactRepository._internal();
 
   /// Returns a Future<List<Contact>> with all available contacts.
-  Future<List<Contact>> fetchContacts() async {
+  Future<ContactBook> fetchContacts() async {
     try {
-      final List<Contact> contacts = LocalDataSource.contacts
-          .map((entry) => _parseContact(entry))
-          .toList();
+      final ContactBook contactBook =
+          ContactBook.from(LocalDataSource.contacts);
+
       return Future.delayed(
-          Duration(milliseconds: futureDelay), () => contacts);
+          Duration(milliseconds: futureDelay), () => contactBook);
     } catch (e) {
       debugPrint(e);
+      return null;
     }
-  }
-
-  Contact _parseContact(String localEntry) {
-    // Checks if the local entry contains any value
-    if (!localEntry.isNotNullNorEmpty) {
-      throw InvalidLocalDataException(data: localEntry);
-    }
-    // Splits the entry by whitespace character
-    List<String> splitStrings = localEntry.split(' ');
-    // Returns a parsed contact with first and last name
-    return Contact(firstName: splitStrings.first, lastName: splitStrings.last);
   }
 }
