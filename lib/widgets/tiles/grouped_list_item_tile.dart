474cec692b84ba7cd9951721cc7c450930f17d7d
Pedro Belfort <belfortdev@gmail.com>
2020-08-22 23:38:19 -0300

    Create styled tile for grouped list items

---
+++ lib/widgets/tiles/grouped_list_item_tile.dart
@@ -0,0 +1,56 @@
+import 'package:ay_flutter_challenge/utils/extensions/string_operations_extension.dart';
+import 'package:ay_flutter_challenge/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+/// A single fixed-height row that displays a title as [RichText].
+/// If [emphasis] is false, then the [title] text is displayed as [Text].
+class GroupedListItemTile extends StatelessWidget {
+  const GroupedListItemTile(
+      {Key key, @required this.title, this.onTap, this.emphasis = true})
+      : assert(title != null),
+        super(key: key);
+
+  final String title;
+  final bool emphasis;
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    print(title);
+
+    final styles = Styles.of(context);
+    return ListTile(dense: true, title: _buildTitle(styles), onTap: onTap);
+  }
+
+  Widget _buildTitle(Styles styles) {
+    if (emphasis) {
+      final richTitle = _RichTitle(title);
+      return RichText(
+        text: TextSpan(
+          style: styles.texts.itemTile,
+          children: <TextSpan>[
+            TextSpan(text: richTitle.firstWord),
+            TextSpan(
+                text: richTitle.remainingWords,
+                style: TextStyle(fontWeight: FontWeight.w600)),
+          ],
+        ),
+      );
+    } else {
+      return Text(title, style: styles.texts.itemTile);
+    }
+  }
+}
+
+class _RichTitle {
+  String firstWord;
+  String remainingWords;
+
+  _RichTitle(String title) {
+    final words = title.split(' ');
+    firstWord = words.isNotEmpty ? words[0] : '';
+    remainingWords = words.length > 1
+        ? ' ' + words.sublist(1)?.join(" ")?.toCapitalCase()
+        : '';
+  }
+}
