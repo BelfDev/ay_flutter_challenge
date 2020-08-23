42edc766dc2a1f3f976bd4f043c10b4d41ec71d2
Pedro Belfort <belfortdev@gmail.com>
2020-08-22 23:39:44 -0300

    Export tiles

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,5 @@
 export 'flexible_sliver_app_bar.dart';
 export 'gradient_filter.dart';
 export 'grouped_list_view.dart';
+export 'tiles/grouped_list_item_tile.dart';
+export 'tiles/grouped_list_section_tile.dart';
