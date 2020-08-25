bf69e8142bd12219957903aaca2781c941332b37
Pedro Belfort <belfortdev@gmail.com>
2020-08-25 13:02:19 -0300

    Add like button

--- lib/widgets/my_contact_tile.dart
+++ lib/widgets/my_contact_tile.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// A customized [ListTile] that displays information about the challenge author (me).
-class MyContactTile extends StatelessWidget {
+class MyContactTile extends StatefulWidget {
   static const String _avatarImageSrc =
       'https://avatars0.githubusercontent.com/u/11461969?s=460&u=357b347fb6e91bac8eb9f1dbb24131434a163dc1&v=4';
   static const String _linkedInUrl =
       'https://www.linkedin.com/in/pedrobelfort/';
+  static const String _trackedGithubUrl = 'https://grabify.link/L9GDWZ';
+
+  @override
+  _MyContactTileState createState() => _MyContactTileState();
+}
+
+class _MyContactTileState extends State<MyContactTile> {
+  bool isLiked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +26,23 @@ class MyContactTile extends StatelessWidget {
       leading: ClipOval(
         child: FadeInImage.assetNetwork(
           fit: BoxFit.cover,
-          image: _avatarImageSrc,
+          image: MyContactTile._avatarImageSrc,
           placeholder: 'assets/images/avatar_placeholder.png',
         ),
       ),
-      trailing: Icon(
-        Icons.favorite_border,
-        color: styles.placeholderIconColor,
-        size: 32,
+      trailing: IconButton(
+        icon: Icon(
+          isLiked ? Icons.favorite : Icons.favorite_border,
+          color: styles.placeholderIconColor,
+          size: 32,
+        ),
+        onPressed: () {
+          setState(() {
+            isLiked = !isLiked;
+            // I'll know you liked me!
+            _launchURL(MyContactTile._trackedGithubUrl);
+          });
+        },
       ),
       title: Text(
         'Pedro Belfort',
@@ -35,12 +52,11 @@ class MyContactTile extends StatelessWidget {
         'Author',
         style: styles.texts.myCardSubtitle,
       ),
-      onTap: () => _launchURL(),
+      onTap: () => _launchURL(MyContactTile._linkedInUrl),
     );
   }
 
-  _launchURL() async {
-    const url = _linkedInUrl;
+  _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
